aca521673be3a53f6b8f7257a0a71fe18d5032d9
Marat Bakirov <lcdswat@deb-virtual>
2017-01-31 00:34:08 -0600

    gallery upload

--- gallery.php
+++ gallery.php
@@ -1,1 +1,54 @@
- 
+<!DOCTYPE html>
+<html>
+<head>
+<title>Gallery | Azamat Abdulmanov</title>
+<?php include($_SERVER['DOCUMENT_ROOT'].'/includes/head.html'); ?>
+<!-- FANCYBOX -->
+<link rel="stylesheet" href="/site-files/fancybox/source/jquery.fancybox.css?v=2.1.6" type="text/css" media="screen" />
+<script type="text/javascript" src="/site-files/fancybox/source/jquery.fancybox.pack.js?v=2.1.6"></script>
+<link rel="stylesheet" href="/site-files/fancybox/source/helpers/jquery.fancybox-thumbs.css?v=1.0.7" type="text/css" media="screen" />
+<script type="text/javascript" src="/site-files/fancybox/source/helpers/jquery.fancybox-thumbs.js?v=1.0.7"></script>
+<script type="text/javascript" src="/site-files/fancybox-load.js"></script>
+</head>
+
+<body>
+
+<!-- Боковое меню -->
+<?php include($_SERVER['DOCUMENT_ROOT'].'/includes/side-menu.html'); ?>
+
+<div class="content">
+	<a class="fancybox-thumb" rel="fancybox-thumb" href="/images/i-01.jpg" title=""><img src="/images/i-01-t.jpg" alt="" /></a>
+	<a class="fancybox-thumb" rel="fancybox-thumb" href="/images/i-02.jpg" title=""><img src="/images/i-02-t.jpg" alt="" /></a>
+	<a class="fancybox-thumb" rel="fancybox-thumb" href="/images/i-03.jpg" title=""><img src="/images/i-03-t.jpg" alt="" /></a>
+	<a class="fancybox-thumb" rel="fancybox-thumb" href="/images/i-04.jpg" title=""><img src="/images/i-04-t.jpg" alt="" /></a>
+	<a class="fancybox-thumb" rel="fancybox-thumb" href="/images/i-05.jpg" title=""><img src="/images/i-05-t.jpg" alt="" /></a>
+	<a class="fancybox-thumb" rel="fancybox-thumb" href="/images/i-06.jpg" title=""><img src="/images/i-06-t.jpg" alt="" /></a>
+	<a class="fancybox-thumb" rel="fancybox-thumb" href="/images/i-07.jpg" title=""><img src="/images/i-07-t.jpg" alt="" /></a>
+	<a class="fancybox-thumb" rel="fancybox-thumb" href="/images/i-08.jpg" title=""><img src="/images/i-08-t.jpg" alt="" /></a>
+	<a class="fancybox-thumb" rel="fancybox-thumb" href="/images/i-09.jpg" title=""><img src="/images/i-09-t.jpg" alt="" /></a>
+	<a class="fancybox-thumb" rel="fancybox-thumb" href="/images/i-10.jpg" title=""><img src="/images/i-10-t.jpg" alt="" /></a>
+	<a class="fancybox-thumb" rel="fancybox-thumb" href="/images/i-11.jpg" title=""><img src="/images/i-11-t.jpg" alt="" /></a>
+	<a class="fancybox-thumb" rel="fancybox-thumb" href="/images/i-12.jpg" title=""><img src="/images/i-12-t.jpg" alt="" /></a>
+	<a class="fancybox-thumb" rel="fancybox-thumb" href="/images/i-13.jpg" title=""><img src="/images/i-13-t.jpg" alt="" /></a>
+	<a class="fancybox-thumb" rel="fancybox-thumb" href="/images/i-14.jpg" title=""><img src="/images/i-14-t.jpg" alt="" /></a>
+	<a class="fancybox-thumb" rel="fancybox-thumb" href="/images/i-15.jpg" title=""><img src="/images/i-15-t.jpg" alt="" /></a>
+	<a class="fancybox-thumb" rel="fancybox-thumb" href="/images/i-16.jpg" title=""><img src="/images/i-16-t.jpg" alt="" /></a>
+	<a class="fancybox-thumb" rel="fancybox-thumb" href="/images/i-17.jpg" title=""><img src="/images/i-17-t.jpg" alt="" /></a>
+	<a class="fancybox-thumb" rel="fancybox-thumb" href="/images/i-18.jpg" title=""><img src="/images/i-18-t.jpg" alt="" /></a>
+	<a class="fancybox-thumb" rel="fancybox-thumb" href="/images/i-19.jpg" title=""><img src="/images/i-19-t.jpg" alt="" /></a>
+	<a class="fancybox-thumb" rel="fancybox-thumb" href="/images/i-20.jpg" title=""><img src="/images/i-20-t.jpg" alt="" /></a>
+	<a class="fancybox-thumb" rel="fancybox-thumb" href="/images/i-21.jpg" title=""><img src="/images/i-21-t.jpg" alt="" /></a>
+	<a class="fancybox-thumb" rel="fancybox-thumb" href="/images/i-22.jpg" title=""><img src="/images/i-22-t.jpg" alt="" /></a>
+	<a class="fancybox-thumb" rel="fancybox-thumb" href="/images/i-23.jpg" title=""><img src="/images/i-23-t.jpg" alt="" /></a>
+	<a class="fancybox-thumb" rel="fancybox-thumb" href="/images/i-24.jpg" title=""><img src="/images/i-24-t.jpg" alt="" /></a>
+	<a class="fancybox-thumb" rel="fancybox-thumb" href="/images/i-25.jpg" title=""><img src="/images/i-25-t.jpg" alt="" /></a>
+	<a class="fancybox-thumb" rel="fancybox-thumb" href="/images/i-26.jpg" title=""><img src="/images/i-26-t.jpg" alt="" /></a>
+	<a class="fancybox-thumb" rel="fancybox-thumb" href="/images/i-27.jpg" title=""><img src="/images/i-27-t.jpg" alt="" /></a>
+	<a class="fancybox-thumb" rel="fancybox-thumb" href="/images/i-28.jpg" title=""><img src="/images/i-28-t.jpg" alt="" /></a>
+	<a class="fancybox-thumb" rel="fancybox-thumb" href="/images/i-29.jpg" title=""><img src="/images/i-29-t.jpg" alt="" /></a>
+	<a class="fancybox-thumb" rel="fancybox-thumb" href="/images/i-30.jpg" title=""><img src="/images/i-30-t.jpg" alt="" /></a>
+	<a class="fancybox-thumb" rel="fancybox-thumb" href="/images/i-31.jpg" title=""><img src="/images/i-31-t.jpg" alt="" /></a>
+</div>
+
+</body>
+</html>
