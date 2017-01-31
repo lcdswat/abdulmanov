68351522716bf37b28080c1d711189cf71117c72
lcdswat <lcd-swat@yandex.ru>
2017-01-31 06:41:27 +0300

    upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Azamat Abdulmanov</title>
+<?php include($_SERVER['DOCUMENT_ROOT'].'/includes/head.html'); ?>
+</head>
+<body>
+
+<!-- Боковое меню -->
+<?php include($_SERVER['DOCUMENT_ROOT'].'/includes/side-menu.html'); ?>
+
+<!-- Серая плашка -->
+<div id="gray-bg"></div>
+
+
+</body>
+</html>
