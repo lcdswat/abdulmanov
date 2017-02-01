11dc8aee27cd587f697bc0b03379252bc12a54cc
Marat Bakirov <lcdswat@deb-virtual>
2017-02-01 07:25:30 -0600

    Upload

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 <link rel="stylesheet" href="/site-files/owlcarousel/dist/assets/owl.carousel.min.css" />
 <?php include($_SERVER['DOCUMENT_ROOT'].'/includes/head.html'); ?>
 <script src="/site-files/owlcarousel/dist/owl.carousel.min.js"></script>
+<script src="//code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 </head>
 <body>
 
@@ -27,8 +28,28 @@
             <span class="customPrevBtn"></span><span class="customNextBtn"></span>
         </div>
 	</div>
+
+	<!-- Текст -->
+	<div class="main-text" id="main-text-one">
+		<span>—</span> Resident of «Top15Ufa: showmen's team» project<br>
+		<span>—</span> Leader of the TV project «Better once to see»<br>
+		<span>—</span> Lead and Speaker «Brides Schools» in Ufa
+	</div>
+	<div class="main-text" id="main-text-two">
+		<span>—</span> Resident of «Top15Ufa: showmen's team» project<br>
+		<span>—</span> Leader of the TV project «Better once to see»<br>
+		<span>—</span> Lead and Speaker «Brides Schools» in Ufa
+	</div>
+	<div class="clean"></div>
+
 </div>
 
+<!-- Скрипт смены текста -->
+<script type="text/javascript">
+
+</script>
+
+<!-- Скрипт слайдер -->
 <script type="text/javascript">
 var owl = $('.owl-carousel');
 owl.owlCarousel({
@@ -36,9 +57,13 @@ owl.owlCarousel({
 });
 $('.customNextBtn').click(function() {
     owl.trigger('next.owl.carousel');
+    $('#main-text-one').hide();
+    $('#main-text-two').show("slide", {direction:"left"}, 400);
 })
 $('.customPrevBtn').click(function() {
     owl.trigger('prev.owl.carousel');
+    $('#main-text-two').hide();
+    $('#main-text-one').show("slide", {direction:"left"}, 400);
 })
 </script>
 
